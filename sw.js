70b40aac3c219a099b60beb3fcc62757cf45f27c
Oj <oj@oojmed.com>
2020-01-28 17:06:44 +0000

    v6.0.0-Beta-5

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const baseCacheName = 'Formuliser';
-const versionName = 'v6.0.0-Beta-4';
+const versionName = 'v6.0.0-Beta-5';
 const cacheName = baseCacheName + '-' + versionName;
 const runtimeCacheName = cacheName + '-runtime';
 

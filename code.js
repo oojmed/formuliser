7b2747fc22c165a503fb853420abbb06b3f47ef9
Oj <oj@oojmed.com>
2020-01-30 10:15:57 +0000

    v6 rc2

--- code.js
+++ code.js
@@ -433,6 +433,23 @@ document.getElementById("refresh").onclick = function() {
   window.location.href = window.location.href;
 }
 
+document.getElementById("install").onclick = function() {
+  showSnackbar();
+
+  deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+
+  deferredPrompt.userChoice
+    .then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      deferredPrompt = null;
+    });
+}
+
 registerSW();
 
 async function registerSW() {
@@ -458,7 +475,7 @@ async function registerSW() {
             switch (newWorker.state) {
               case 'installed':
                 if (navigator.serviceWorker.controller) {
-                  document.getElementById("update").className = "show"; // Show Update Prompt
+                  showSnackbar("update"); // Show Update Prompt
                 }
 
                 break;
@@ -471,3 +488,35 @@ async function registerSW() {
     }
   }
 }
+
+let snackbars = ["update", "install"];
+
+function showSnackbar(name) {
+  let c = document.getElementById("snackbar").className;
+
+  document.getElementById("snackbar").className = "";
+
+  if (name !== undefined) {
+    setTimeout(function() {
+      for (const s of snackbars.filter(x => x !== name)) {
+        document.getElementById(`snackbar-${s}`).className = "";
+      }
+
+      document.getElementById("snackbar").className = "show";
+
+      document.getElementById("snackbar-" + name).className = "show";
+    }, c === "" ? 0 : 1000);
+  }
+}
+
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 76 and later from showing the mini-infobar
+  e.preventDefault();
+
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+
+  showSnackbar("install");
+});
